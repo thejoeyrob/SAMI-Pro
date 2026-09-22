76e6dcee24b51b2a3283aaf87b4f215c9d06abd8
thejoeyrob <josephrwhelan@icloud.com>
2026-09-22 20:14:58 +0100

    Regenerate version.js for 2.7.17

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 /* Generated from VERSION.json by build.mjs. */
-window.SAMI_VERSION=Object.freeze({version:"2.7.14",descriptor:"FIELD_TOUCH_SERVICES_UI",asset:name=>name.split('?')[0]+'?v=2.7.14'});
+window.SAMI_VERSION=Object.freeze({version:"2.7.17",descriptor:"TOUCH_TARGET_PASS_1",asset:name=>name.split('?')[0]+'?v=2.7.17'});
