ef87ff9b5aee7273fc957a57bc6556aa25be3189
JW E.D.S. <josephrwhelan@icloud.com>
2026-09-23 23:26:55 +0100

    Regenerate version.js for 2.7.19

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 /* Generated from VERSION.json by build.mjs. */
-window.SAMI_VERSION=Object.freeze({version:"2.7.18",descriptor:"PRECISION_ASSETS_TOPBAR_FIX",asset:name=>name.split('?')[0]+'?v=2.7.18'});
+window.SAMI_VERSION=Object.freeze({version:"2.7.19",descriptor:"RESTORE_CONSISTENT_BUILD",asset:name=>name.split('?')[0]+'?v=2.7.19'});
